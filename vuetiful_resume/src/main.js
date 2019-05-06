91e5fff77015222a02be88adea584ad513bb2515
key kim <keyhyuk.kim@gmail.com>
2019-05-06 14:17:20 +0000

    Initalize Repository

---
+++ vuetiful_resume/src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+
+
+Vue.config.productionTip = false
+Vue.use(Vuetify);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  Vuetify,
+  components: { App },
+  template: '<App/>'
+})
