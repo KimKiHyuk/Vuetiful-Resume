c03d0d09cf638285ff02171d9403f8ae0849cd15
key Kim <keyhyuk.kim@gmail.com>
2019-06-14 14:26:20 +0000

    Add layout at skill view

--- vuetiful_resume/src/view/Skill.vue
+++ vuetiful_resume/src/view/Skill.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container>
-    <v-layout wrap>
+    <v-layout row wrap>
       <v-flex xs12 sm6 md4 lg3 v-for="(skill, i) in skills" :key="i">
         <div class="text-xs-center ma-4">
           <v-avatar :size="avatarSize" color="indigo" :tile="tile">
             <img :src="skill.imageSource" alt="avatar">
           </v-avatar>
+          <div>
+            <div class="title mt-1 mb-1">{{ skill.name }}</div>
+            <v-rating small :readonly="true" v-model="skill.proficiency"></v-rating>
+          </div>
+					<div>
+						<span class="blue lighten-4 ma-1 font-weight-bold" v-for="(element, j) in skill.hashtag" :key="j">
+							#{{ element.name }}
+						</span>
+					</div>
         </div>
       </v-flex>
     </v-layout>
@@ -20,6 +29,7 @@ export default {
       skills: [
         {
           name: "C#",
+          proficiency: 5,
           imageSource: "https://vuetifyjs.com/apple-touch-icon-180x180.png",
           hashtag: [
             {
@@ -32,6 +42,7 @@ export default {
         },
         {
           name: "JAVA",
+          proficiency: 5,
           imageSource: "https://vuetifyjs.com/apple-touch-icon-180x180.png",
           hashtag: [
             {
@@ -41,6 +52,7 @@ export default {
         },
         {
           name: "C/C++",
+          proficiency: 5,
           imageSource: "https://vuetifyjs.com/apple-touch-icon-180x180.png",
           hashtag: [
             {
@@ -53,6 +65,7 @@ export default {
         },
         {
           name: "javascript",
+          proficiency: 3,
           imageSource: "https://vuetifyjs.com/apple-touch-icon-180x180.png",
           hashtag: [
             {
@@ -65,6 +78,7 @@ export default {
         },
         {
           name: "python",
+          proficiency: 5,
           imageSource: "https://vuetifyjs.com/apple-touch-icon-180x180.png",
           hashtag: [
             {
@@ -77,6 +91,7 @@ export default {
         },
         {
           name: "Scala",
+          proficiency: 4,
           imageSource: "https://vuetifyjs.com/apple-touch-icon-180x180.png",
           hashtag: [
             {
