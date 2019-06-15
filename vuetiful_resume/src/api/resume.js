89db2b074a2c295cd78b2e7b0f53c253d3f6214d
key Kim <keyhyuk.kim@gmail.com>
2019-06-15 14:29:39 +0000

    Add mock api

---
+++ vuetiful_resume/src/api/resume.js
@@ -0,0 +1,15 @@
+const mockAboutme = {
+  information: {
+    name: '김기혁',
+    job: '소프트웨어 개발자',
+    greet: 'Hello there11!',
+    nation: '대한민국 서울특별시',
+    introduce:
+      'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.'
+  }
+}
+
+export default {
+  name: 'api',
+  fetchDataFromServer: () => mockAboutme
+}
