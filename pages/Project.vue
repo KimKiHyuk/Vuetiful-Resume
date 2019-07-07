d2126b9508276cf33446a299585a7e275c30c422
key Kim <keyhyuk.kim@gmail.com>
2019-07-07 11:19:45 +0000

    Open project list always because it is more pretty

--- pages/Project.vue
+++ pages/Project.vue
@@ -1,41 +1,49 @@
 <template>
   <v-container>
-    <v-expansion-panel expand class="mb-4" v-for="(item,i) in allInformation" :key="i">
-      <v-expansion-panel-content>
-        <template v-slot:header>
-          <div class="title font-weight-black">{{ item.category }}</div>
-        </template>
-        <v-divider></v-divider>
-        <v-card class="pa-2">
-          <v-expansion-panel inset expand>
-            <v-expansion-panel-content v-for="(item,i) in item.project" :key="i">
-              <template v-slot:header>
-                <div class="subheading font-weight-bold">{{ item.title }}</div>
-              </template>
-              <v-card color="grey lighten-4">
-                <v-card-text>
-                  <div class="body-2 ml-3">{{ item.description }}</div>
-                </v-card-text>
-                <v-card color="grey lighten-3">
+    <v-card class="elevation-2 text-xs-right blue-grey lighten-5 pa-3">
+      <v-expansion-panel
+        expand
+        v-model="panel[i]"
+        class="elevation-3 mb-4"
+        v-for="(item,i) in allInformation"
+        :key="i"
+      >
+        <v-expansion-panel-content>
+          <template v-slot:header>
+            <div class="title font-weight-black">{{ item.category }}</div>
+          </template>
+          <v-divider></v-divider>
+          <v-card class="pa-2">
+            <v-expansion-panel inset expand>
+              <v-expansion-panel-content v-for="(item,i) in item.project" :key="i">
+                <template v-slot:header>
+                  <div class="subheading font-weight-bold">{{ item.title }}</div>
+                </template>
+                <v-card color="grey lighten-4">
                   <v-card-text>
-                    <div
-                      class="body-2 ml-3"
-                      v-for="(tech, j) in item.tech"
-                      :key="j"
-                    >{{ tech.skill }} : {{ tech.usage }}</div>
+                    <div class="body-2 ml-3">{{ item.description }}</div>
                   </v-card-text>
-                  <v-card-actions>
-                    <v-flex class="text-xs-right">
-                      <v-btn flat color="orange" @click="openGithub(item.github)">Github</v-btn>
-                    </v-flex>
-                  </v-card-actions>
+                  <v-card color="grey lighten-3">
+                    <v-card-text>
+                      <div
+                        class="body-2 ml-3"
+                        v-for="(tech, j) in item.tech"
+                        :key="j"
+                      >{{ tech.skill }} : {{ tech.usage }}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-flex class="text-xs-right">
+                        <v-btn flat color="orange" @click="openGithub(item.github)">Github</v-btn>
+                      </v-flex>
+                    </v-card-actions>
+                  </v-card>
                 </v-card>
-              </v-card>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-card>
   </v-container>
 </template>
 
@@ -53,7 +61,9 @@ export default {
     this.$store.dispatch('projectStore/fetchInformation');
   },
   data() {
-    return {};
+    return {
+      panel: [...Array(2)].map(() => [true]), // this is little bit agly.. let me find a better way
+    };
   },
 };
 </script>
