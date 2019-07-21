e77acaadf6194c3d19e9f33b70796e28bfcd3da3
key Kim <keyhyuk.kim@gmail.com>
2019-07-21 06:15:09 +0000

    Add language base button

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,40 +1,46 @@
 <template>
   <v-container>
-    <nav>
-      <v-toolbar flat app>
-        <v-toolbar-side-icon @click="drawer = !drawer"/>
-        <v-toolbar-title>
-          <span>Key Resume</span>
-        </v-toolbar-title>
-      </v-toolbar>
+    <v-layout>
+      <nav>
+        <v-toolbar flat app>
+          <v-toolbar-side-icon @click="drawer = !drawer"/>
+          <v-toolbar-title>
+            <span>Key Resume</span>
+          </v-toolbar-title>
+          <v-spacer/>
+          <v-btn class="hidden-sm-and-down" @click="getKorean" :disabled="!languageButtonSwitch">한국어</v-btn>
+          <v-btn class="hidden-sm-and-down" @click="getEnglish" :disabled="languageButtonSwitch">English</v-btn>
+          <div> {{ languageButtonSwitch }} </div>
+        </v-toolbar>
 
-      <v-navigation-drawer v-model="drawer" app>
-        <v-list class="pa-0">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg">
-            </v-list-tile-avatar>
+        <v-navigation-drawer v-model="drawer" app>
+          <v-list class="pa-0">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg">
+              </v-list-tile-avatar>
 
-            <v-list-tile-content>
-              <v-list-tile-title>ki-hyuk KIM</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+              <v-list-tile-content>
+                <v-list-tile-title>ki-hyuk KIM</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
 
-        <v-list class="pt-0" dense>
-          <v-divider/>
-          <v-list-tile v-for="item in items" :key="item.title" nuxt-link :to="item.router">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
+          <v-list class="pt-0" dense>
+            <v-divider/>
+            <v-list-tile v-for="item in items" :key="item.title" nuxt-link :to="item.router">
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
 
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-    </nav>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
+      </nav>
+    </v-layout>
   </v-container>
 </template>
 
@@ -42,6 +48,7 @@
 export default {
   data() {
     return {
+      languageButtonSwitch: false,
       drawer: false,
       items: [
         { title: 'AboutMe', icon: 'assignment_ind', router: '/AboutMe' },
@@ -51,6 +58,20 @@ export default {
         { title: 'Project', icon: 'work', router: '/Project' },
       ],
     };
+  },
+  methods: {
+    getKorean() {
+      console.log('kor');
+      console.log(this.languageButtonSwitch);
+      // eslint-disable-line no-unused-vars
+      this.languageButtonSwitch = false;
+    },
+    getEnglish() {
+      console.log('eng');
+      console.log(this.languageButtonSwitch);
+      // eslint-disable-line no-unused-vars
+      this.languageButtonSwitch = true;
+    },
   },
 };
 </script>
