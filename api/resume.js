3b57b149b42b1dcc773a62a7d0140dbccd5da7fd
key kim <keyhyuk.kim@gmail.com>
2019-08-29 18:51:39 +0900

    Change api address and refactor

--- api/resume.js
+++ api/resume.js
@@ -1,63 +1,13 @@
 import axios from 'axios';
 
-export const API_LIST = {
-  aboutme: 'http://keykim.iptime.org:8081/aboutme',
-  career: 'http://keykim.iptime.org:8081/career',
-  education: 'http://keykim.iptime.org:8081/education',
-  skill: 'http://keykim.iptime.org:8081/skill',
-  project: 'http://keykim.iptime.org:8081/project',
-};
-// eslint-disable-next-line no-unused-vars
-const mockAboutme = {
-  information: {
-    name: 'Key',
-    job: 'software developer',
-    greet: 'Hello there11!',
-    nation: '대한민국 서울특별시',
-    introduce:
-      'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.',
-  },
-};
+const baseUrl = 'http://127.0.0.1:8081';
 
-// eslint-disable-next-line no-unused-vars
-const mockCareer = {
-  information: [
-    {
-      color: 'cyan',
-      year: '1960',
-      month: '1',
-      company: 'google inc',
-      experience: 'honey sucker',
-    },
-    {
-      color: 'green',
-      year: '1970',
-      month: '2',
-      company: 'microsoft',
-      experience: '아무것도 안함',
-    },
-    {
-      color: 'pink',
-      year: '1980',
-      month: '3',
-      company: 'naver',
-      experience: '월급 축냄',
-    },
-    {
-      color: 'amber',
-      year: '1990',
-      month: '4',
-      company: 'kakao',
-      experience: '업무시간에 잠',
-    },
-    {
-      color: 'orange',
-      year: '2000',
-      month: '5',
-      company: 'ROKCC',
-      experience: '인턴',
-    },
-  ],
+export const API_LIST = {
+  aboutme: baseUrl.concat('/aboutme'),
+  career: baseUrl.concat('/career'),
+  education: baseUrl.concat('/education'),
+  skill: baseUrl.concat('/skill'),
+  project: baseUrl.concat('/project'),
 };
 
 export async function fetchAllFromServer(api) {
