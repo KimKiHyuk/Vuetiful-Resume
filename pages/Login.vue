f44a77fdc2d10375dfcb39aa935569740680f6d5
key kim <keyhyuk.kim@gmail.com>
2019-09-02 19:07:50 +0900

    Add login page

---
+++ pages/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <div>
+      <h2>Login</h2>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <div>
+          <v-text-field
+            v-model="username"
+            :counter="1024"
+            :rules="nameRules"
+            label="username"
+            required
+          ></v-text-field>
+        </div>
+
+        <div>
+          <v-text-field
+            v-model="password"
+            :counter="1024"
+            :rules="passwordRules"
+            label="password"
+            required
+          ></v-text-field>
+        </div>
+
+        <div>
+          <v-btn :disabled="!valid" color="green" @click="login">Validate</v-btn>
+        </div>
+      </v-form>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      valid: false,
+      username: '',
+      nameRules: [v => !!v || 'Name is required'],
+      password: '',
+      passwordRules: [v => !!v || 'password is required'],
+    };
+  },
+  methods: {
+    login() {
+      if (this.$refs.form.validate()) {
+        // eslint-disable-next-line indent
+        // commit
+      }
+    },
+  },
+};
+</script>
