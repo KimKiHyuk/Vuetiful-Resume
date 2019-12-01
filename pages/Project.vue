ad956177ebf2843d4f27c14cd312ef0f64063ad8
key kim <keyhyuk.kim@gmail.com>
2019-12-02 01:19:07 +0900

    Update project page

--- pages/Project.vue
+++ pages/Project.vue
@@ -1,69 +1,87 @@
 <template>
   <v-container>
-    <v-card class="elevation-2 text-xs-right blue-grey lighten-5 pa-3">
-      <v-expansion-panel
-        expand
-        v-model="panel[i]"
-        class="elevation-3 mb-4"
-        v-for="(item,i) in allInformation"
-        :key="i"
-      >
-        <v-expansion-panel-content>
-          <template v-slot:header>
-            <div class="title font-weight-black">{{ item.category }}</div>
-          </template>
-          <v-divider></v-divider>
-          <v-card class="pa-2">
-            <v-expansion-panel inset expand>
-              <v-expansion-panel-content v-for="(item,i) in item.project" :key="i">
-                <template v-slot:header>
-                  <div class="subheading font-weight-bold">{{ item.title }}</div>
-                </template>
-                <v-card color="grey lighten-4">
-                  <v-card-text>
-                    <div class="body-2 ml-3">{{ item.description }}</div>
-                  </v-card-text>
-                  <v-card color="grey lighten-3">
-                    <v-card-text>
-                      <div
-                        class="body-2 ml-3"
-                        v-for="(tech, j) in item.tech"
-                        :key="j"
-                      >{{ tech.skill }} : {{ tech.usage }}</div>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-flex class="text-xs-right">
-                        <v-btn flat color="orange" @click="openGithub(item.github)">Github</v-btn>
-                      </v-flex>
-                    </v-card-actions>
-                  </v-card>
-                </v-card>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-card>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md4 lg3 v-for="(data, i) in meta" :key="i">
+        <v-card :color="data.color" class="text-xs-right ma-2 pa-2" outlined max-width="344">
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="overline mb-4">{{ data.name}}</div>
+              <v-list-item-title class="headline mb-1">{{data.title}}</v-list-item-title>
+              <v-list-item-subtitle>{{data.subtitle}}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-card-actions>
+              <div class="text-xs-right">
+                <v-btn @click="openGithub(data.link)">Listen Now</v-btn>
+              </div>
+            </v-card-actions>
+          </v-list-item>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
-
 export default {
   name: 'Project',
-  methods: mapActions('projectStore', ['fetchInformation']),
-  openGithub: (url) => {
-    window.open(url, '_blank');
-  },
-  computed: mapGetters('projectStore', ['allInformation']),
-  created() {
-    this.$store.dispatch('projectStore/fetchInformation');
-    this.panel = [...Array(this.$store.getters['projectStore/allInformation'].length)].map(() => [true]);
+  methods: {
+    openGithub: url => {
+      window.open(url, '_blank');
+    },
   },
   data() {
     return {
       panel: [],
+      meta: [
+        {
+          title: 'Vue.js App',
+          subtitle:
+            'Vue.js, 자바스크립트를 활용해 만든 프론트엔드 웹 어플리케이션',
+          color: 'purple',
+          name: 'Web',
+          link: 'www.github.com',
+        },
+        {
+          title: 'Backend using kakk',
+          subtitle:
+            'Vue.js, 자바스크립트를 활용해 만든 프론트엔드 웹 어플리케이션',
+          color: 'red',
+          name: 'App',
+          link: 'www.github.com',
+        },
+        {
+          title: 'frontend tech',
+          subtitle:
+            'Vue.js, 자바스크립트를 활용해 만든 프론트엔드 웹 어플리케이션',
+          color: 'red',
+          name: '3',
+          link: 'www.github.com',
+        },
+        {
+          title: 'deep learnming',
+          subtitle:
+            'Vue.js, 자바스크립트를 활용해 만든 프론트엔드 웹 어플리케이션',
+          color: 'blue',
+          name: '4',
+          link: 'www.github.com',
+        },
+        {
+          title: 'data pipeline',
+          subtitle:
+            'Vue.js, 자바스크립트를 활용해 만든 프론트엔드 웹 어플리케이션',
+          color: 'pink',
+          name: '5',
+          link: 'www.github.com',
+        },
+        {
+          title: 'dart app',
+          subtitle:
+            'Vue.js, 자바스크립트를 활용해 만든 프론트엔드 웹 어플리케이션',
+          color: 'purple',
+          name: '6',
+          link: 'www.github.com',
+        },
+      ],
     };
   },
 };
