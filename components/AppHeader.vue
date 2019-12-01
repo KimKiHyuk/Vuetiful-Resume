19eaf4ee8209d6e8a945020bad6e793a42831c3f
key kim <keyhyuk.kim@gmail.com>
2019-12-02 01:17:23 +0900

    Update header considering new vuetify dependency

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,40 +1,36 @@
 <template>
   <v-container>
-    <nav>
-      <v-toolbar flat app>
-        <v-toolbar-side-icon @click="drawer = !drawer"/>
-        <v-toolbar-title>
-          <span>Key Resume</span>
-        </v-toolbar-title>
-      </v-toolbar>
+    <v-app-bar color="deep-purple accent-4" dark app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My files</v-toolbar-title>
+    </v-app-bar>
 
-      <v-navigation-drawer v-model="drawer" app>
-        <v-list class="pa-0">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg">
-            </v-list-tile-avatar>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list class="pa-0">
+        <v-list-item>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+          </v-list-item-avatar>
 
-            <v-list-tile-content>
-              <v-list-tile-title>ki-hyuk KIM</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+          <v-list-item-content>
+            <v-list-item-title>ki-hyuk KIM</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
 
-        <v-list class="pt-0" dense>
-          <v-divider/>
-          <v-list-tile v-for="item in items" :key="item.title" nuxt-link :to="item.router">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
+      <v-list-item-group class="pt-0" dense>
+        <v-divider />
+        <v-list-item v-for="item in items" :key="item.title" nuxt-link :to="item.router">
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
 
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-    </nav>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-navigation-drawer>
   </v-container>
 </template>
 
