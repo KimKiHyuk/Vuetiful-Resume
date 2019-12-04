ffec6cdc069bdec4a8cd0d6b70bf0e1ec1d334c9
key kim <keyhyuk.kim@gmail.com>
2019-12-04 19:20:35 +0900

    Update footer link

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,36 +1,50 @@
 <template>
-  <v-footer absolute dark padless>
-    <v-card flat tile class="indigo lighten-1 white--text text-center">
-      <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
-      </v-card-text>
-
-      <v-card-text
-        class="white--text pt-0"
-      >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
-
-      <v-divider></v-divider>
-
-      <v-card-text class="white--text">
+  <v-footer color="primary lighten-1" padless absolute>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="element in links"
+        :key="element.link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        @click="openUrl(element.link)"
+      >
+        <v-icon size="24px">{{ element.icon }}</v-icon>
+      </v-btn>
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
         {{ new Date().getFullYear() }} —
         <strong>Vuetify</strong>
-      </v-card-text>
-    </v-card>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
-
 <script>
 export default {
+  methods: {
+    openUrl: url => {
+      window.open(url, '_blank');
+    },
+  },
   data: () => ({
-    icons: [
-      'fab fa-facebook',
-      'fab fa-twitter',
-      'fab fa-google-plus',
-      'fab fa-linkedin',
-      'fab fa-instagram',
+    links: [
+      {
+        icon: 'fab fa-github',
+        link: 'https://www.github.com/kimkihyuk',
+      },
+      {
+        icon: 'fab fa-linkedin',
+        link: 'https://www.linkedin.com/in/kihyuk-kim-176a33139',
+      },
+      {
+        icon: 'fab fa-medium',
+        link: 'https://medium.com/@keyhyuk.kim',
+      },
+      {
+        icon: 'fab fa-facebook',
+        link: 'https://www.facebook.com/profile.php?id=100001299898804',
+      },
     ],
   }),
 };
