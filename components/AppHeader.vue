43d48413100a9ebb7464e39aaf52a51859380914
key kim <keyhyuk.kim@gmail.com>
2019-12-04 20:46:51 +0900

    Adjust color

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-app-bar color="deep-purple accent-4" dark app>
+    <v-app-bar color="black" dark app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Resume</v-toolbar-title>
     </v-app-bar>
