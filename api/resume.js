cb6c1cc41566d31479b7390206831d3ed9a6d912
key kim <keyhyuk.kim@gmail.com>
2019-12-07 13:41:38 +0900

    private commit

--- api/resume.js
+++ api/resume.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const baseUrl = 'secret key';
+const baseUrl = 'https://key-resume-gateway.azurewebsites.net/resume';
 
 export const API_LIST = {
-  aboutme: baseUrl.concat('/aboutme'),
-  career: baseUrl.concat('/career'),
-  education: baseUrl.concat('/education'),
-  skill: baseUrl.concat('/skill'),
-  project: baseUrl.concat('/project'),
-  format: baseUrl.concat('/format'),
+  aboutme: baseUrl.concat('/aboutme/last'),
+  career: baseUrl.concat('/career/last'),
+  education: baseUrl.concat('/education/last'),
+  skill: baseUrl.concat('/skill/last'),
+  project: baseUrl.concat('/project/last'),
+  format: baseUrl.concat('/format/last'),
 };
 
 export async function fetchAllFromServer(api) {
