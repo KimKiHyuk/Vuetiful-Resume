f6b4e08a642033c94c1c02b706bee3640f98c032
key kim <keyhyuk.kim@gmail.com>
2019-12-09 00:30:10 +0900

    Add html line parser

--- pages/AboutMe.vue
+++ pages/AboutMe.vue
@@ -17,7 +17,7 @@
           <v-card-title class="justify-center">
             <div class="headline">{{ allInformation.greet }}</div>
           </v-card-title>
-          <v-card-text>{{ allInformation.introduce }}</v-card-text>
+          <v-card-text v-html="htmlParseHelper(allInformation.introduce)"></v-card-text>
         </v-flex>
       </v-layout>
     </v-card>
@@ -26,10 +26,20 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import newLine2Br from '../utility/htmlParser';
 
 export default {
   name: 'AboutMe',
-  methods: mapActions('aboutMeStore', ['fetchInformation']),
+  methods: {
+    htmlParseHelper: json => newLine2Br(json),
+  },
+  ...mapActions('aboutMeStore', ['fetchInformation']),
   computed: mapGetters('aboutMeStore', ['allInformation']),
 };
 </script>
+
+<style>
+.pre-formatted {
+  white-space: pre;
+}
+</style>
