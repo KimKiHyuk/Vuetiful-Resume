a204e91dc3dada3e57183c5c87af4255c086fadb
key kim <keyhyuk.kim@gmail.com>
2019-12-09 22:15:11 +0900

    Update api

--- api/resume.js
+++ api/resume.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
-// const baseUrl = 'https://key-resume-gateway.azurewebsites.net/resume';
+const baseUrl = 'https://key-resume-gateway.azurewebsites.net/resume';
 
-const baseUrl = 'http://127.0.0.1:8085';
+//const baseUrl = 'http://127.0.0.1:8085';
 export const API_LIST = {
-  aboutme: baseUrl.concat('/aboutme'),
-  career: baseUrl.concat('/career'),
-  education: baseUrl.concat('/education'),
-  skill: baseUrl.concat('/skill'),
-  project: baseUrl.concat('/project'),
-  format: baseUrl.concat('/format'),
+  aboutme: baseUrl.concat('/aboutme/last'),
+  career: baseUrl.concat('/career/last'),
+  education: baseUrl.concat('/education/last'),
+  skill: baseUrl.concat('/skill/last'),
+  project: baseUrl.concat('/project/last'),
+  format: baseUrl.concat('/format/last'),
 };
 
 export async function fetchAllFromServer(api) {
   const response = await axios.get(api);
-  console.log('new api : ', api);
   return response;
 }
