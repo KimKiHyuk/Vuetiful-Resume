107f2bba07e7777912c339bf3a9892e46b7c18a7
key kim <keyhyuk.kim@gmail.com>
2019-12-19 00:07:58 +0900

    Update skill page
    hash tag removed

--- pages/Skill.vue
+++ pages/Skill.vue
@@ -1,33 +1,35 @@
 <template>
-  <v-container>
+  <v-container v-if="allInformation !== undefined">
     <v-layout row wrap>
       <v-flex xs12 sm6 md4 lg3 v-for="(skill, i) in allInformation" :key="i">
         <div class="text-center ma-5">
           <v-avatar :size="120" color="transparent">
-            <img :src="skill.imageSource" alt="avatar" />
+            <img :src="skill.fields.image" alt="avatar" />
           </v-avatar>
           <div>
-            <div class="title mt-1 mb-1">{{ skill.name }}</div>
-            <v-rating small :readonly="true" v-model="skill.proficiency"></v-rating>
-          </div>
-          <div>
-            <span
-              class="blue lighten-4 ma-1 font-weight-bold"
-              v-for="(element, j) in skill.hashTags"
-              :key="j"
-            >#{{ element.tag }}</span>
+            <div class="title mt-1 mb-1">{{ skill.fields.name }}</div>
+            <v-rating
+              small
+              :readonly="true"
+              v-model="skill.fields.proficiency"
+            ></v-rating>
           </div>
         </div>
       </v-flex>
     </v-layout>
   </v-container>
+  <Loading v-else />
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import Loading from '../components/Loading.vue';
 
 export default {
   name: 'Skill',
+  components: {
+    Loading,
+  },
   methods: mapActions('skillStore', ['fetchInformation']),
   computed: mapGetters('skillStore', ['allInformation']),
   created() {
@@ -36,5 +38,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
