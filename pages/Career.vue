44276d495b3a9c4746b10d74ff5e0eea25897b39
key kim <keyhyuk.kim@gmail.com>
2019-12-19 00:06:02 +0900

    Update career page

--- pages/Career.vue
+++ pages/Career.vue
@@ -1,31 +1,47 @@
 <template>
-  <v-container>
+  <v-container v-if="allInformation !== undefined">
     <v-timeline dense>
-      <v-timeline-item v-for="(career, i) in allInformation" :key="i" color="indigo accent-4" large>
+      <v-timeline-item
+        v-for="(career, i) in allInformation"
+        :key="i"
+        color="indigo accent-4"
+        large
+      >
         <v-card class="elevation-5">
           <v-card-title class="headline">
-            {{ career.company }}
-            <span class="subheading ml-3">{{ career.period }}</span>
+            {{ career.fields.company }}
+            <span class="subheading ml-3"
+              >{{ career.fields.period_start.slice(0, 7) }} ~
+              {{ career.fields.period_end.slice(0, 7) }}</span
+            >
           </v-card-title>
 
           <v-card-text>
             <div>
-              <p class="body-2">{{ career.position }}</p>
-              <p v-html="htmlParseHelper(career.experience)" class="body-1"></p>
+              <p class="body-2">{{ career.fields.position }}</p>
+              <p
+                v-html="htmlParseHelper(career.fields.experience)"
+                class="body-1"
+              ></p>
             </div>
           </v-card-text>
         </v-card>
       </v-timeline-item>
     </v-timeline>
   </v-container>
+  <Loading v-else />
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import Loading from '../components/Loading.vue';
 import newLine2Br from '../utility/htmlParser';
 
 export default {
   name: 'Career',
+  components: {
+    Loading,
+  },
   methods: { htmlParseHelper: json => newLine2Br(json) },
   ...mapActions('careerStore', ['fetchInformation']),
   computed: mapGetters('careerStore', ['allInformation']),
@@ -38,5 +54,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
