ed916c759a0a04ccb5706025bb9b3e2e68263979
key kim <keyhyuk.kim@gmail.com>
2019-12-19 00:09:41 +0900

    Change api address

--- api/resume.js
+++ api/resume.js
@@ -2,14 +2,12 @@ import axios from 'axios';
 
 const baseUrl = 'https://key-resume-gateway.azurewebsites.net/resume';
 
-//const baseUrl = 'http://127.0.0.1:8085';
 export const API_LIST = {
-  aboutme: baseUrl.concat('/aboutme/last'),
-  career: baseUrl.concat('/career/last'),
-  education: baseUrl.concat('/education/last'),
-  skill: baseUrl.concat('/skill/last'),
-  project: baseUrl.concat('/project/last'),
-  format: baseUrl.concat('/format/last'),
+  aboutme: baseUrl.concat('/aboutme'),
+  career: baseUrl.concat('/career'),
+  education: baseUrl.concat('/education'),
+  skill: baseUrl.concat('/skill'),
+  project: baseUrl.concat('/project'),
 };
 
 export async function fetchAllFromServer(api) {
