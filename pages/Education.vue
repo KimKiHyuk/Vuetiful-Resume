5835cacd8ff0635fefd09d22a21c36a3415a4d2e
key kim <keyhyuk.kim@gmail.com>
2019-12-19 00:07:15 +0900

    Add education page

--- pages/Education.vue
+++ pages/Education.vue
@@ -1,33 +1,49 @@
 <template>
-  <v-container>
+  <v-container v-if="allInformation !== undefined">
     <v-timeline dense>
-      <v-timeline-item v-for="(edu, i) in allInformation" :key="i" color="blue lighten-2" large>
+      <v-timeline-item
+        v-for="(edu, i) in allInformation"
+        :key="i"
+        color="blue lighten-2"
+        large
+      >
         <v-card class="elevation-5">
           <v-card-title class="headline">
-            {{ edu.insititute }}
-            <span class="subheading ml-3">{{ edu.period }}</span>
+            {{ edu.fields.insititute }}
+            <span class="subheading ml-3"
+              >{{ edu.fields.period_start.slice(0, 7) }} ~
+              {{ edu.fields.period_end.slice(0, 7) }}</span
+            >
           </v-card-title>
 
           <v-card-text>
             <div>
-              <p class="body-2">{{ edu.type }}</p>
-              <p v-html="htmlParseHelper(edu.description)" class="body-1"></p>
+              <p class="body-2">{{ edu.fields.type }}</p>
+              <p
+                v-html="htmlParseHelper(edu.fields.description)"
+                class="body-1"
+              ></p>
             </div>
           </v-card-text>
         </v-card>
       </v-timeline-item>
     </v-timeline>
   </v-container>
+  <Loading v-else />
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import newLine2Br from '../utility/htmlParser';
+import Loading from '../components/Loading.vue';
 
 export default {
   name: 'Education',
   methods: {
     htmlParseHelper: json => newLine2Br(json),
+  },
+  components: {
+    Loading,
   },
   ...mapActions('educationStore', ['fetchInformation']),
   computed: mapGetters('educationStore', ['allInformation']),
@@ -39,5 +55,4 @@ export default {
   },
 };
 </script>
-<style>
-</style>
+<style></style>
